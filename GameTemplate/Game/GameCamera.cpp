8b1a88b9de6b9c12147b1a4d45cf10b3439d20bd
YuMild <YuMild0375@softbank.ne.jp>
2022-02-07 12:23:44 +0900

    2/7 ikayariengineの追加

--- GameTemplate/Game/GameCamera.cpp
+++ GameTemplate/Game/GameCamera.cpp
@@ -16,10 +16,45 @@ GameCamera::~GameCamera()
 bool GameCamera::Start()
 {
 	m_player = FindGO<Player>("player");
+	m_toCameraPos.Set(0.0f, 100.0f, 100.0f);
+
 	return true;
 }
 
 void GameCamera::Update()
 {
 	m_target = m_player->GetPosition();
+	m_target.y += 100.0f;
+	m_target += g_camera3D->GetForward() * 10.0f;
+
+	Vector3 toCameraPosOld = m_toCameraPos;
+
+	//	パッドの入力を使ってカメラを回す
+	float x = g_pad[0]->GetRStickXF();
+	float y = g_pad[0]->GetRStickYF();
+
+	//	Y軸周りの回転
+	Quaternion qRot;
+	qRot.SetRotationDeg(Vector3::AxisY, 2.0f * x);
+	qRot.Apply(m_toCameraPos);
+
+	//	X軸周りの回転
+	Vector3 axisX;
+	axisX.Cross(Vector3::AxisY, m_toCameraPos);
+	axisX.Normalize();
+	qRot.SetRotationDeg(axisX, 2.0f * y);
+	qRot.Apply(m_toCameraPos);
+
+	//	カメラの回転の上限をチェックする
+	//	注視点から視点までのベクトルを正規化する
+	Vector3 toPosDir = m_toCameraPos;
+	toPosDir.Normalize();
+	if (toPosDir.y < -0.5f) {
+		m_toCameraPos = toCameraPosOld;							//	カメラが上に向き過ぎるのを防ぐ
+	}
+	else if (toPosDir.y > 1.0f) {
+		m_toCameraPos = toCameraPosOld;							//	カメラが下に向き過ぎるのを防ぐ
+	}
+	//	視点を計算する
+	Vector3 pos = m_target + m_toCameraPos;
 }
