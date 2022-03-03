04897d207e450dbaa48cb55031c0da78b4f85204
ikayari <owaatesimaatano@icloud.com>
2022-03-03 16:31:43 +0900

    3/3 16:32 豊岡 ばねカメラ追加　画像規格1600 900に統一

--- GameTemplate/k2EngineLow/k2EngineLowPreCompile.h
+++ GameTemplate/k2EngineLow/k2EngineLowPreCompile.h
@@ -84,6 +84,7 @@ using namespace Microsoft::WRL;
 #include "graphics/GaussianBlur.h"
 #include "Bloom.h"
 #include "RenderingEngine.h"
+#include "SpringCamera.h"
 
 const UINT FRAME_BUFFER_W = 1600;	// フレームバッファの幅。
 const UINT FRAME_BUFFER_H = 900;	// フレームバッファの高さ。
