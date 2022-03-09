e8f392766933596565b42bc40fca3dfddc0bb2ee
YuMild <YuMild0375@softbank.ne.jp>
2022-03-09 13:42:15 +0900

    3/9 13:40 好村 ステージデータのローポリ化

--- GameTemplate/Game/BackGround.cpp
+++ GameTemplate/Game/BackGround.cpp
@@ -3,13 +3,13 @@
 
 bool BackGround::Start()
 {
-	m_stage_Ground.Init("Assets/modelData/Stage1/Stage_1_Ground.tkm");
-	m_stage_Wall.Init("Assets/modelData/Stage1/Stage_1_wall.tkm");
-	m_stage_Bumpers.Init("Assets/modelData/Stage1/Stage_1_bumpers.tkm");
+	m_stage_Ground.Init("Assets/modelData/Stage1/Stage1.tkm");
+	//m_stage_Wall.Init("Assets/modelData/Stage1/Stage_1_wall.tkm");
+	//m_stage_Bumpers.Init("Assets/modelData/Stage1/Stage_1_bumpers.tkm");
 	// 静的物理オブジェクトを作成。
 	m_stage_Ground_PSO.CreateFromModel(m_stage_Ground.GetModel(), m_stage_Ground.GetModel().GetWorldMatrix(),0.0f);
-	m_stage_Wall_PSO.CreateFromModel(m_stage_Wall.GetModel(), m_stage_Wall.GetModel().GetWorldMatrix(),0.8f);
-	m_stage_Bumpers_PSO.CreateFromModel(m_stage_Bumpers.GetModel(), m_stage_Bumpers.GetModel().GetWorldMatrix(), 1.2f);
+	//m_stage_Wall_PSO.CreateFromModel(m_stage_Wall.GetModel(), m_stage_Wall.GetModel().GetWorldMatrix(),0.8f);
+	//m_stage_Bumpers_PSO.CreateFromModel(m_stage_Bumpers.GetModel(), m_stage_Bumpers.GetModel().GetWorldMatrix(), 1.2f);
 
 	return true;
 }
@@ -22,6 +22,6 @@ void BackGround::Update()
 void BackGround::Render(RenderContext& rc)
 {
 	m_stage_Ground.Draw(rc);
-	m_stage_Wall.Draw(rc);
-	m_stage_Bumpers.Draw(rc);
+	//m_stage_Wall.Draw(rc);
+	//m_stage_Bumpers.Draw(rc);
 }
